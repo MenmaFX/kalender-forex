571a5165e4757c93523cb62aa4aed9e059dddffc
MenmaFX <mahallaris@gmail.com>
2026-09-17 22:19:42 +0700

    Switch MainActivity to stable Java to bypass Kotlin compiler error

---
+++ android/app/src/main/java/com/menmafx/calendar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.menmafx.calendar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/menmafx/calendar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.menmafx.calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
